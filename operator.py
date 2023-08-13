9bd8d1d608f1cce87e78c2d96f84a818029deaae
gsubham001 <142078672+gsubham001@users.noreply.github.com>
2023-08-13 21:48:17 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,25 @@
+#day 2 ques-1
+# n1=2
+# n2=4
+# sum =n1 +n2
+# print(sum)
+
+
+# day 2 question 2
+# n1=4
+# n2=6
+# subtract=n2 - n1
+# print(subtract)
+
+
+# day 2 question 3
+# n1=2
+# n2=3
+# multiple=n1*n2
+# print(multiple)
+
+#dat 2 question 4
+# n1=4
+# n2=2
+# divide=n1/n2
+# print(divide)
