036a1525e7a0a5266cd0b379ac5ced63882904f3
gsubham001 <142078672+gsubham001@users.noreply.github.com>
2023-08-18 02:00:05 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,8 @@
+def concatenate_lists(list1, list2):
+    concatenated_list = list1 + list2
+    return concatenated_list
+
+list1 = [0,5,8]
+list2 = [4,9,8]
+result = concatenate_lists(list1, list2)
+print("Concatenated List:", result)
