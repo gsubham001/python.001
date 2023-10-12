bad71d2a70f44a7ccba487f4156be7f0e86a41ee
gsubham001 <142078672+gsubham001@users.noreply.github.com>
2023-10-12 12:23:37 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,19 @@
+#defining element
+
+mylist=['yes','no','true' ,'false']
+print(mylist)
+
+#covert list into set
+
+mylist = ['1','2','3']
+myset=set(mylist)
+print(myset)
+
+# conversion of set into list
+
+myset={'1','2','3'}
+mylist=list(myset)
+print(mylist)
+
+print(myset)
+print(mylist)
